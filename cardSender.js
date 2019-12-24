3466aa6fa2681ba2613e815bb1709e04807808fe
Ricardo Perdomo <hndgd1980@gmail.com>
2019-12-23 21:54:05 -0600

    missing sms connection

--- cardSender.js
+++ cardSender.js
@@ -1,38 +1,65 @@
 $(document).ready(function () {
 
     //Setting value of current Month,Date,Year,Hour,Minutes,Seconds
-    let now = moment().format('MMMM Do YYYY, h:mm:ss a');
-    console.log(now);
+    let now = moment().format('MMMM Do YYYY, h:mm a');
+    // console.log(now);
 
     //Capturing the event of the XMAS button
-    let xMasButton = $("#xmasButton")
-    console.log(xMasButton);
+    let $xMasButton = $("#xmasButton")
+    // console.log($xMasButton);
+
+    //Event listener
+    $xMasButton.on("click", function () {
+        console.log("Listener is firing");
+        sendText(now);
+    });
 
     //Value of the reciever
-    let textReceiver = 8162779412
-    console.log(textReceiver);
+    let sender = '8162779412'
+    // console.log(sender);
 
     //Setting value of the specific instance you want to send the text
-    let fixedDate = "December 23rd 2019, 1:57:00 pm"
+    let fixedDate = "December 23rd 2019, 9:26 pm"
+
+    //person object
+    let textee = {
+        sender: "Ricardo Perdomo",
+        reciever: "",
+        text: "Merry Christmas and a Happy New Year!!!"
+    }
 
     //Function that sends the text
     function sendText(now) {
         if (now == fixedDate) {
+            // Setting form
+            let from = textee.sender;
+            let to = '18162779412';
+            const text = textee.text;
 
-            console.log("sending text at " + now);
+
+
+            $("#sent_text").text("Text was sent successfully!!!")
+
         } else {
-            console.log("error");
+            console.log("Current time " + now);
+            console.log("Fixed time   " + fixedDate);
+            $("#sent_text").text("sendText Function error --- fixedDate is not equal to " + now)
 
         }
 
     }
-
-    sendText(now);
-
+});
 
 
 
 
 
-})
+
+
+
+
+
+
+
+
 
